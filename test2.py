a9cff21d9ccbad8b8b74de80623de57cfee3e3e5
zhouyunqiang <1204364027@qq.com>
2017-10-15 01:05:02 +0800

    3

--- test2.py
+++ test2.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python
-
-fruits = ['banana', 'apple',  'mango']
-for index in range(len(fruits)):
-   print 'Current fruit :', fruits[index]
+import os
+file_list=os.listdir(r'E:\PPT')
+print(file_list)
+saved_patch=os.getcwd()
+print('current workingdirectory is'+saved_patch)
+os.chdir(r'E:\PPT')
+for index in range(len(file_list)) :
+ print('old name'+file_list[index])
+ #print('new name'+'file_list[index].tranalate(None,'0123456789'))
+ os.renames(file_list[index],file_list[index].translate(None,'0123456789'))
+os.chdir(saved_patch)
--- test3.py
+++ test3.py
@@ -1,13 +1,11 @@
 import os
-def test3():
- file_list=os.listdir(r'E:\PPT')
- print(file_list)
- saved_patch=os.getcwd()
- print('current workingdirectory is'+saved_patch)
- os.chdir(r'E:\PPT')
-   for file_name in file_list:
-     print('old name'+file_name)
-     print('new name'+filename.tranalate(None,'0123456789'))
-     os.renames(file_name,file_name.translate(None,'0123456789'))
-   os.chdir(saved_patch)
-test3()
+file_list=os.listdir(r'E:\PPT')
+print(file_list)
+saved_patch=os.getcwd()
+print('current workingdirectory is'+saved_patch)
+os.chdir(r'E:\PPT')
+for index in range(len(file_list)) :
+ print('old name'+file_list[index])
+ #print('new name'+'file_list[index].tranalate(None,'0123456789'))
+ os.renames(file_list[index],file_list[index].translate(None,'0123456789'))
+os.chdir(saved_patch)
